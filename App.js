d6632989f96289e7bf56db440141be89fa7e6f7f
SuomiPH <suomiponce@protonmail.com>
2020-03-02 15:14:30 +0100

    add MesMariagesScreen

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import MesMariagesScreen from './screens/MesMariagesScreen'
+
 export default function App() {
   return (
     <View style={styles.container}>
+    	<MesMariagesScreen/>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
--- screens/MesMariagesScreen.js
+++ screens/MesMariagesScreen.js
@@ -1,11 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import {connect} from 'react-redux';
 
+import { 
+	View, ScrollView, SafeAreaView,
+	Text, StyleSheet  } from 'react-native';
+
+import { Icon, ListItem } from 'react-native-elements';
+
+
+
+
 function MesMariagesScreen() {
 	
+	// nav haut
 	
-
-
+	return(
+		<SafeAreaView>
+			<View 
+				constainerStyle={{
+					backgroundColor: '#FAEBE4', 
+					flex:1, alignItems:'center', justifyContent:'center'
+				}}>
+				<Icon name='ios-menu' type='ionicon' color='#555753' />
+				<Text>Mes Mariages</Text>
+				
+				
+				
+				
+				<ListItem
+				  title='Mariage John & Janet'
+				  subtitle={
+				    <View style={styles.subtitleView}>
+				      <Icon name='clockcircleo' type='antDesign' color='grey' />
+				      <Text style={styles.ratingText}>10/02/2020</Text>
+				      <Icon name='location-on' type='materialIcons' color='grey' />
+				      <Text style={styles.ratingText}>5 months ago</Text>
+				    </View>
+				  }
+				  rightAvatar={{ source: require('../images/picture-1.jpg') }}
+				/>
+				
+				
+				
+				
+			</View>
+		</SafeAreaView>
+	
+	)
+	
+	styles = StyleSheet.create({
+  subtitleView: {
+    flexDirection: 'row',
+    paddingLeft: 10,
+    paddingTop: 5
+  },
+  ratingText: {
+    paddingLeft: 10,
+    color: 'grey'
+  }
 }
 
 
