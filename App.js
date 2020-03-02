61ef506de2693919536e6596cbe51586925b15da
SuomiPH <suomiponce@protonmail.com>
2020-03-02 16:13:31 +0100

    adds some style to the MesMarriagesScreen

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <View style={styles.container}>
     	<MesMariagesScreen/>
-      <Text>Open up App.js to start working on your app!</Text>
+      
     </View>
   );
 }
--- screens/MesMariagesScreen.js
+++ screens/MesMariagesScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {connect} from 'react-redux';
+//import { connect } from 'react-redux';
 
 import { 
 	View, ScrollView, SafeAreaView,
@@ -17,70 +17,127 @@ function MesMariagesScreen() {
 	return(
 		<SafeAreaView>
 			<View 
-				constainerStyle={{
+				style={{
+					width:'100%', height:'50px',
 					backgroundColor: '#FAEBE4', 
-					flex:1, alignItems:'center', justifyContent:'center'
+					flexDirection: 'row', alignItems: 'center',
 				}}>
-				<Icon name='ios-menu' type='ionicon' color='#555753' />
-				<Text>Mes Mariages</Text>
+				<Icon name='menu' type='material' color='grey' />
+				<Text style={{paddingLeft: 20}}>
+					Mes Mariages
+				</Text>
+			</View>
 				
 				
 				
+				<ListItem key={1}
+				  title='Mariage John & Janet'
+				  subtitle={
+				    <View style={styles.subtitleView}>
+					    <Icon name='clock' type='evilicon' color='grey' />
+					    <Text style={styles.ratingText}>10/02/2020</Text>
+					    <View style={styles.interSpace}/>
+				      <Icon name='location-on' type='materialIcons' color='grey' />
+				      <Text style={styles.ratingText}>5 months ago</Text>
+				    </View>
+				  }
+				  rightAvatar={{ source: require('../assets/picture-1.jpg') }}
+				  leftAvatar={{ source: require('../assets/picture-2.jpg') }}
+				  titleStyle={{paddingTop: 10, paddingLeft: 20}}
+				  bottomDivider
+				/>
 				
 				<ListItem
 				  title='Mariage John & Janet'
 				  subtitle={
 				    <View style={styles.subtitleView}>
-				      <Icon name='clockcircleo' type='antDesign' color='grey' />
-				      <Text style={styles.ratingText}>10/02/2020</Text>
+					    <Icon name='clock' type='evilicon' color='grey' />
+					    <Text style={styles.ratingText}>10/02/2020</Text>
+					    <View style={styles.interSpace}/>
 				      <Icon name='location-on' type='materialIcons' color='grey' />
 				      <Text style={styles.ratingText}>5 months ago</Text>
 				    </View>
 				  }
-				  rightAvatar={{ source: require('../images/picture-1.jpg') }}
+				  rightAvatar={{ source: require('../assets/picture-1.jpg') }}
+				  leftAvatar={{ source: require('../assets/picture-2.jpg') }}
+				  titleStyle={{paddingTop: 10, paddingLeft: 20}}
+				  bottomDivider
+				/>
+				
+				<ListItem
+				  title='Mariage John & Janet'
+				  titleStyle={{paddingTop: 10, paddingLeft: 20}}
+				  subtitle={
+				    <View style={styles.subtitleView}>
+					    <Icon name='clock' type='evilicon' color='grey' />
+					    <Text style={styles.ratingText}>10/02/2020</Text>
+					    <View style={styles.interSpace}/>
+				      <Icon name='location-on' type='materialIcons' color='grey' />
+				      <Text style={styles.ratingText}>5 months ago</Text>
+				    </View>
+				  }
+				  rightAvatar={{ source: require('../assets/picture-1.jpg') }}
+				  leftAvatar={{ source: require('../assets/picture-2.jpg') }}
+				  
+				  bottomDivider
 				/>
 				
 				
 				
-				
-			</View>
 		</SafeAreaView>
 	
 	)
 	
-	styles = StyleSheet.create({
-  subtitleView: {
-    flexDirection: 'row',
-    paddingLeft: 10,
-    paddingTop: 5
-  },
-  ratingText: {
-    paddingLeft: 10,
-    color: 'grey'
-  }
+
 }
 
 
-function mapStateToProps(state) {
-  return { 
-//  	photoUrlList: state.photoUrlList,
-//  	username: state.username,
-  }
-}
+const styles = StyleSheet.create({
+	subtitleView: {
+		flexDirection: 'row',
+		alignItems: 'end',
+		paddingLeft: 15,
+		paddingTop: 5
+	},
+	ratingText: {
+		paddingLeft: 10,
+		paddingRight: 10,
+		color: 'grey'
+	},
+	interSpace:{
+		paddingLeft: 30,
+	}
+});
 
-function mapDispatchToProps(dispatch) {
-  return {
-//    setPhotoUrlList: function ( photo_url_list ) { 
-//       dispatch( { type: 'setLocalList', list: photo_url_list } ) 
-//    },
-//    deletePhotoUrl: function (i) {
-//    	 dispatch( {type: 'deleteUrl', index: i } )
-//    }
-  }
-}
+
+export default MesMariagesScreen; 
+
+//function mapStateToProps(state) {
+//  return { 
+////  	photoUrlList: state.photoUrlList,
+////  	username: state.username,
+//  }
+//}
+
+//function mapDispatchToProps(dispatch) {
+//  return {
+////    setPhotoUrlList: function ( photo_url_list ) { 
+////       dispatch( { type: 'setLocalList', list: photo_url_list } ) 
+////    },
+////    deletePhotoUrl: function (i) {
+////    	 dispatch( {type: 'deleteUrl', index: i } )
+////    }
+//  }
+////}
 
 
-export default connect(
-  null, 
-  null
-)( MesMariagesScreen );
+//export default connect(
+//  null, 
+//  null
+//)( MesMariagesScreen );
+
+
+
+
+
+
