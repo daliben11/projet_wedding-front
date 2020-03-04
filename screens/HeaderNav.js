92ab808a63356785bbe77d02ab32ef08deab3a93
SuomiPH <suomiponce@protonmail.com>
2020-03-04 11:15:18 +0100

    commit before pulling header nav

--- screens/HeaderNav.js
+++ screens/HeaderNav.js
@@ -3,15 +3,14 @@ import {Header} from 'react-native-elements';
 
 export default function HeaderNav(){
     return(
-            <Header
-                leftComponent={{ icon: 'menu', color: '#000' }}
-                centerComponent={{ text: 'Mon Profil ', style: {fontFamily:'greatvibes', color: '#000', fontSize:30 } }}
-                containerStyle={{
-                    backgroundColor: '#FAEBE4',
-                    height: 84,
-                    
-                }}
-            />
+        <Header
+            leftComponent={{ icon: 'menu', color: '#000' }}
+            centerComponent={{ text: 'Mon Profil ', style: {fontFamily:'greatvibes', color: '#000', fontSize:30 } }}
+            containerStyle={{
+                backgroundColor: '#FAEBE4',
+                height: 84,    
+            }}
+        />
 
     )
 }
