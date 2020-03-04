8b0701ccf6d4ea436e68eef1bee60ce53069ede3
SuomiPH <suomiponce@protonmail.com>
2020-03-04 12:39:12 +0100

    fix after pull/push on AddWedding

--- reducers/mariage.reducer.js
+++ reducers/mariage.reducer.js
@@ -1,10 +1,7 @@
 export default function ( mariageInfo = {}, action ) {
 	
 	
-	if ( action.type === 'setWeddingDate' ) {
-		mariageInfo.date = action.date;
-
-	} else if ( action.type === 'setMyWedding' ) {
+	if ( action.type === 'setMyWedding' ) {
 		
 	}
 	
