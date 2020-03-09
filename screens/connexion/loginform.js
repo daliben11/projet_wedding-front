88f5ee8fdcf38c79372b16fde2411a00ade43cbf
Dali <benrabah.mohamedali@gmail.com>
2020-03-09 12:40:05 +0100

    Ajout de l'adresse heroku

--- screens/connexion/loginform.js
+++ screens/connexion/loginform.js
@@ -18,7 +18,7 @@ function Loginform( props ) {
         }
 
         else {
-        let data = await fetch("https://tranquil-journey-96536.herokuapp.com/sign-in",{
+        let data = await fetch("https://weedingplanner.herokuapp.com/sign-in",{
             method: 'POST',
             headers: {'Content-Type':'application/x-www-form-urlencoded'},
             body: `email=${email}&password=${password}`
