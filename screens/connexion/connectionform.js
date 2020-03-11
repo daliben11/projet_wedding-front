f6321f75c6cbfe05d2e5468b21a6d306a078b073
sackda75 <dsouks@yahoo.fr>
2020-03-11 15:50:30 +0100

    pb fixé car bouton Me connecter n'apparaissait pas dans petits écrans

--- screens/connexion/connectionform.js
+++ screens/connexion/connectionform.js
@@ -28,8 +28,9 @@ function Connectionform( props ) {
             />
 
             <Button title="Me connecter" 
+                  style={styles.buttonLinks} 
 		          type="clear"
-		          titleStyle={{ color: 'pink', fontSize: 30, marginTop: 20}}
+		          titleStyle={{ color: 'pink', fontSize: 30, marginTop: 1}}
 		          onPress={ () => props.toSignIn() }
             />
         
@@ -43,7 +44,7 @@ const styles = StyleSheet.create({
         padding: 40
      },
     buttonLinks: {
-        marginBottom: 50,
+        marginBottom: 25,
     },
 })
 
